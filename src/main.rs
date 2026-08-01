f7a6dcbb2c1ff8eaaba98771abaf5dadc5c545b4
okaduki <okaduki1@gmail.com>
2026-08-01 14:41:56 +0900

    implement dataset

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io::Read, path::Path};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
 
 use crate::tokenizer::{BPETokenizer, END_TOKEN, train_bpe};
 
@@ -30,21 +34,57 @@ fn ch01_training_tokenizer() {
 }
 
 fn ch01_tokenizer_check() {
+    let input_path = Path::new("./data/tiny_codes.txt");
     let output_path = Path::new("./data/merge_rules.cbor");
     let vocab_size = 1000;
 
     let tokenizer = BPETokenizer::load_from(output_path, END_TOKEN).unwrap();
 
-    for id in 256..280 {
+    for id in 256..266 {
         println!("{} -> '{}'", id, tokenizer.decode(&[id]));
     }
 
     for id in 990..vocab_size {
         println!("{} ->  '{}'", id, tokenizer.decode(&[id]));
     }
+
+    {
+        let mut input_file = File::open(input_path).unwrap();
+        let mut text = String::new();
+        input_file.read_to_string(&mut text).unwrap();
+
+        let original_text: String = text.chars().take(10000).collect();
+        let original_binary = original_text.as_bytes();
+        let encoded = tokenizer.encode(&original_text);
+
+        println!(
+            "binary size: {} / {} ({:.2} % compressed.)",
+            encoded.len(),
+            original_binary.len(),
+            (encoded.len() as f64) / (original_binary.len() as f64) * 100.0,
+        );
+    }
+}
+
+fn ch01_convert_binary() {
+    let input_path = Path::new("./data/tiny_codes.txt");
+    let output_path = Path::new("./data/tiny_codes.bin");
+    let rule_path = Path::new("./data/merge_rules.cbor");
+
+    let tokenizer = BPETokenizer::load_from(rule_path, END_TOKEN).unwrap();
+
+    let mut input_file = File::open(input_path).unwrap();
+    let mut text = String::new();
+    input_file.read_to_string(&mut text).unwrap();
+    let text: String = text.chars().collect();
+
+    let binary = tokenizer.encode(&text);
+    let output_file = File::create(output_path).unwrap();
+    into_writer(&binary, output_file).unwrap();
 }
 
 fn main() {
     ch01_training_tokenizer();
     ch01_tokenizer_check();
+    ch01_convert_binary();
 }
